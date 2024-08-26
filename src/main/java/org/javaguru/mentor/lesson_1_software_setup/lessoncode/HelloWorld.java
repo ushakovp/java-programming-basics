396351f5948bda4648f091bc1f90efe2c4e5190a
javagurulv <javagurulv@gmail.com>
2024-08-26 20:57:23 +0300

    initial commit

--- src/main/java/org/javaguru/mentor/lesson_1_software_setup/lessoncode/HelloWorld.java
+++ src/main/java/org/javaguru/mentor/lesson_1_software_setup/lessoncode/HelloWorld.java
@@ -2,10 +2,9 @@ package org.javaguru.mentor.lesson_1_software_setup.lessoncode;
 
 class HelloWorld {
 
-
     public static void main(String[] args) {
         System.out.println("Valera");
         System.out.println("Kovs");
-
     }
+
 }
